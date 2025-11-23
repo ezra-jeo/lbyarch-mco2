55abf7deb8453ab30604d8b731b6663e5f9b4270
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2025-11-23 17:42:44 +0800

    chore: reorganized file structure

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "./utils/utility.c"
+
+extern double* imgCvtGrayIntToDouble(int height, int width, unsigned char *input);
+
+int main(int argc, char *argv[]) { 
+    int height, width;
+    int i, j; // Loop variables
+    unsigned char* input;
+    double* output;
+
+    // Input
+    scanf("%d %d", &height, &width);
+    input = getImg(height, width);
+
+    output = imgCvtGrayIntToDouble(height, width, input);
+
+    printf("\nInput: \n");
+    printf("%d %d\n", height, width);
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            printf("%hhu ", input[i * width + j]);
+            
+        }
+        printf("\n");
+    }
+
+    printf("\nOutput: \n"); 
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            printf("%0.2lf ", output[i * width + j]);  
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
